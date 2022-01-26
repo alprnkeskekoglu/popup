bd7a876d41adc31e57684e95b9beba7b8ac63010
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2022-01-26 23:08:40 +0300

    popup service done. type1 modal blade done

---
+++ src/Http/Services/PopupService.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Dawnstar\Popup\Http\Services;
+
+use Carbon\Carbon;
+use Dawnstar\Core\Foundation\Dawnstar;
+use Dawnstar\Popup\Models\Popup;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\DB;
+use Jenssegers\Agent\Agent;
+
+class PopupService
+{
+    protected Dawnstar $dawnstar;
+    protected $date;
+
+    public function __construct()
+    {
+        $this->dawnstar = dawnstar();
+        $this->date = date('Y-m-d');
+    }
+
+    public function init()
+    {
+        $popups = $this->getPopups();
+
+        $html = '';
+        foreach ($popups as $popup) {
+            if($this->setLimit($popup)) {
+                continue;
+            }
+
+            $html .= view('PopupWeb::types.' . $popup->type, compact('popup'))->render();
+        }
+
+        return $html;
+    }
+
+    private function getPopups()
+    {
+        $device = $this->getDevice();
+
+        return Popup::where('status', 1)
+            ->where('website_id', $this->dawnstar->website->id)
+            ->whereHas('translation')
+            ->where(function ($q) {
+                $q->where('start_date', null)
+                    ->orWhere('start_date', '<=', $this->date);
+            })
+            ->where(function ($q) {
+                $q->where('end_date', null)
+                    ->orWhere('end_date', '>=', $this->date);
+            })
+            ->where('devices', 'like', "%{$device}%")
+            ->where(function ($q) {
+                $q->where('display', 1)
+                    ->orWhere(function ($query) {
+                        $isHomePage = $this->dawnstar->container->key == 'homepage' ? 1 : 0;
+                        $query->where('display', 2)->where(DB::raw(1), $isHomePage);
+                    })
+                    ->orWhere(function ($query) {
+                        $query->where('display', 3)
+                            ->whereHas('urls', function ($que) {
+                                $que->where('id', $this->dawnstar->url->id);
+                            });
+                    });
+            })
+            ->with('urls', 'translation')
+            ->get();
+    }
+
+    private function getDevice()
+    {
+        $agent = new Agent();
+        return $agent->deviceType();
+    }
+
+    #region Helpers
+    private function setLimit(Popup $popup)
+    {
+        if($popup->limit == 2) {
+            $sessionData = session('_dapo', []);
+            if(isset($sessionData[$popup->id])) {
+                if($sessionData[$popup->id] >= $popup->limit_count) {
+                    return true;
+                }
+                $sessionData[$popup->id]++;
+            } else {
+                $sessionData[$popup->id] = 1;
+            }
+            session(['_dapo' => $sessionData]);
+
+        } elseif($popup->limit == 3) {
+            $cookieData = \Cookie::get('_dapo') ? decrypt(\Cookie::get('_dapo')) : [];
+            $minutes = 60*24*30;
+
+            if(isset($cookieData[$popup->id])) {
+                if($cookieData[$popup->id] >= $popup->limit_count) {
+                    return true;
+                }
+                $cookieData[$popup->id]++;
+            } else {
+                $cookieData[$popup->id] = 1;
+            }
+
+            Cookie::queue(Cookie::make('_dapo', encrypt($cookieData), $minutes));
+        }
+
+        return false;
+    }
+    #endregion
+}
---
+++ src/Resources/views/web/types/1.blade.php
@@ -0,0 +1,41 @@
+<div class="modal fade" id="popupModal{{ $popup->id }}" tabindex="-1" aria-labelledby="modal{{ $popup->id }}Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                @if($popup->show_name)
+                    <h5 class="modal-title" id="modal{{ $popup->id }}Label">{{ $popup->translation->name }}</h5>
+                @endif
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                {!! $popup->translation->detail !!}
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    var modal = new bootstrap.Modal(document.getElementById("popupModal{{ $popup->id }}"))
+    @if($popup->trigger == 1)
+    setTimeout(function () {
+        modal.show();
+    }, 1000);
+    @elseif($popup->trigger == 2)
+    setTimeout(function () {
+        modal.show();
+    }, {{$popup->trigger_count*1000}});
+    @elseif($popup->trigger == 3)
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > {{ $popup->trigger_count }}) {
+            modal.show();
+        }
+    });
+    @endif
+
+    @if($popup->display_second)
+    @php($second = $popup->trigger == 2 ? $popup->trigger_count : 1)
+    setTimeout(function () {
+        modal.hide();
+    }, {{ ($popup->display_second + $second) * 1000}});
+    @endif
+</script>
